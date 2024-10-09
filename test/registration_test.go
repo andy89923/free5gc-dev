cfde6a44ec31c320c9caf7a62142ad2ff1221911
CTFang@WireLab <ctfang.cs12@nycu.edu.tw>
2024-10-09 04:28:26 +0000

    feat: use r17 models in NewRanUe

--- test/registration_test.go
+++ test/registration_test.go
@@ -34,8 +34,8 @@ import (
 	"github.com/free5gc/nas/security"
 	"github.com/free5gc/ngap"
 	"github.com/free5gc/ngap/ngapType"
-	"github.com/free5gc/openapi/Npcf_PolicyAuthorization"
-	"github.com/free5gc/openapi/models"
+	"github.com/free5gc/openapi-r17/models"
+	Npcf_PolicyAuthorization "github.com/free5gc/openapi-r17/pcf/PolicyAuthorization"
 	"github.com/free5gc/util/httpwrapper"
 	"github.com/free5gc/util/milenage"
 )
@@ -2490,8 +2490,10 @@ func TestAFInfluenceOnTrafficRouting(t *testing.T) {
 	client := Npcf_PolicyAuthorization.NewAPIClient(cfg)
 
 	appSessCtx := TestPolicyAuthorization.GetPostAppSessionsData_AFInfluenceOnTrafficRouting()
-
-	_, _, err = client.ApplicationSessionsCollectionApi.PostAppSessions(context.Background(), appSessCtx)
+	postAppSesionReq := &Npcf_PolicyAuthorization.PostAppSessionsRequest{
+		AppSessionContext: &appSessCtx,
+	}
+	_, err = client.ApplicationSessionsCollectionApi.PostAppSessions(context.Background(), postAppSesionReq)
 	assert.Nil(t, err, "PolicyAuthorize failed")
 
 	time.Sleep(1 * time.Second)
